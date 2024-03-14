259a992eda3624327b3be1c1c5a996718948cf97
yonatkim <raenyonat@gmail.com>
2024-03-14 20:48:12 +0200

    Added challenge

--- Week3/Day3/challenge.py
+++ Week3/Day3/challenge.py
@@ -9,24 +9,93 @@ class Circle:
             if 'diameter' in kwargs.items():
                 self.diameter = val
 
-    def get_dimension(self, *args):
-        for key in args.items():
-            if 'radius' in key:
-                return {'radius': self.radius}
-            if 'diameter' in args.items():
-                return {'diameter': self.diameter}
+    def set_dimension(self, *args):
+        '''Sets the radius of diameter of the circle
+        expected two params:
+        str: radius or diameter
+        int or float: a number'''
+        if 'radius' in args: 
+            self.radius = float(args[1]) 
+            print(f"The radius: {self.radius} cm")
+        if 'diameter' in args:
+            self.diameter = float(args[1])
+            print(f"The diameter: {self.diameter} cm")
 
     def get_area(self):
+        '''Calculates the area of the circle
+        updates the class attribute and returns the area'''
         area = 0
         if self.radius != 0:
-            area = math.pi * self.radius ** 2
+            self.area = math.pi * self.radius ** 2
         elif self.diameter != 0:
-            area = math.pi * self.diameter * 2
+            self.area = math.pi * self.diameter * 2
         return area
+    
+    def summarise(self):
+        '''Prints the dimensions of the circle'''
+        if self.radius == 0 and self.diameter != 0:
+            self.radius = self.diameter / 2
+        elif self.radius != 0 and self.diameter == 0:
+            self.diameter = self.radius * 2
+        print(f"The circle's properties are:\n radius: {self.radius}\n diameter: {self.diameter}\n area: {self.area:.2f}")
+
+    def __repr__(self) -> str:
+        '''Prints the class details'''
+        #cl = self.__class__
+        #print(f"<{cl.__name__}.{cl.__text_signature__} object> \n")
+        return f"<Circle radius:{self.radius} diameter:{self.diameter}>"
+
+    def __str__(self):
+        return f"Circle radius:{self.radius} diameter:{self.diameter}"
+
+    def __add__(self, other):
+        '''Add the radius of two circles together'''
+        return (self.diameter/2) + other.radius if self.radius == 0 and self.diameter != 0 else self.radius + other.radius
+
+    def __gt__(self, other):
+        '''Compares the radius of two circles 
+        returns boolean True or False''' 
+        return self.radius > other.radius 
+    
+    def __eq__(self, other):
+        '''Compares the radius of two circles 
+        returns boolean True or False''' 
+        return self.radius == other.radius
+
+    def sort(self, others: list):
+        '''Sorts a list of circles from smallest to largest'''
+        unsorted = [other.radius for other in others]
+        unsorted.append(self.radius)
+        unsorted.sort()
+        return unsorted
         
-Print the attributes of the circle - use a dunder method
-Be able to add two circles together, and return a new circle with the new radius - use a dunder method
-Be able to compare two circles to see which is bigger, and return a Boolean - use a dunder method
-Be able to compare two circles and see if there are equal, and return a Boolean- use a dunder method
-Be able to put them in a list and sort them
-Bonus (not mandatory) : Install the Turtle module, and draw the sorted circles
+
+# test cases
+print('challenge')
+circles = [{'radius': 3}, {'diameter': 5}, {'radius': 7}]
+print(f"The input circles\n {circles}")
+print()
+c0 = Circle(**circles[0])
+print("Circle 0")
+c0.set_dimension(*circles[0], list(circles[0].values())[0])
+c0.get_area()
+c0.summarise()
+print(c0)
+print()
+print("Circle 1")
+c1 = Circle(**circles[1])
+c1.set_dimension(*circles[1], list(circles[1].values())[0])
+c1.get_area()
+c1.summarise()
+print(c1)
+print(f"Is circle 0 greater than  circle 1? {c0 > c1}")
+print()
+print("Circle 2")
+c2 = Circle(**circles[2])
+c2.set_dimension(*circles[2], list(circles[2].values())[0])
+c2.get_area()
+c2.summarise()
+print(c2)
+print(f"Is circle 1 equal to circle 2? {c1 == c2}")
+print(f"Circle 0 added to circle 2 = {c0 + c2} cm")
+print(f"Sorted circles: {c0.sort([c1, c2])}")
---
+++ Week3/Day5/Untitled-1.py
@@ -0,0 +1,8 @@
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    x = 3
+    y = 4
+    result = multiply(x, y)
+    print(f"The result of multiplying {x} and {y} is: {result}")
