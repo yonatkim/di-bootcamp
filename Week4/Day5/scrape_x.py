9281e6dcdad38c7524db9cbd7075416e3dba0216
yonatkim <raenyonat@gmail.com>
2024-03-24 15:29:49 +0200

    Update data

--- Week4/Day5/scrape_x.py
+++ Week4/Day5/scrape_x.py
@@ -1,11 +1,12 @@
 import requests
+import csv
 
 # Twitter API Bearer Token
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAD60swEAAAAASlY0jl4OpDUmT5Ky0sP5dG4abLM%3DMuyxrFQB9uxGYwXFI6BA2IuFrjh794AlOJxBzgJAGmaWFyXIK0'
 
 # Twitter API endpoint for searching tweets
-#endpoint_url = 'https://api.twitter.com/2/tweets/search/recent'
-endpoint_url = 'https://api.twitter.com/2/tweets/'
+endpoint_url = 'https://api.twitter.com/2/tweets/search/recent'
+#endpoint_url = 'https://api.twitter.com/2/tweets/'
 
 # Function to scrape tweets
 def scrape_tweets(query, max_results):
@@ -27,12 +28,19 @@ def scrape_tweets(query, max_results):
         return None
 
 # Example usage
-query = "anime"
+query = "antisemitic content"
 max_results = 10  # Number of tweets to scrape
 tweets_data = scrape_tweets(query, max_results)
 
 # Print scraped tweets
 if tweets_data:
-    for tweet in tweets_data['data']:
-        text = tweet['text']
-        print(f"Tweet: {text}\n")
+    data = {}
+    with open('data.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        for tweet in tweets_data['data']:
+            data.update(tweet['text'])
+        csvwriter.writerows(data)
+ 
+   
+
+        
