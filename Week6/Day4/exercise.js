4adbc44c732b47701a6e74e384243b8c9181aa5a
yonatkim <raenyonat@gmail.com>
2024-04-07 15:11:35 +0300

    added GET-POST to same page and stringify to prettify

---
+++ Week6/Day4/exercise.js
@@ -0,0 +1,34 @@
+/** exercise 3 */
+const marioGame = {
+    detail: "An amazing game!",
+    characters: {
+        mario: {
+            description: "Small and jumpy. Likes princesses.",
+            height: 10,
+            weight: 3,
+            speed: 12,
+        },
+        bowser: {
+            description: "Big and green, Hates princesses.",
+            height: 16,
+            weight: 6,
+            speed: 4,
+        },
+        princessPeach: {
+            description: "Beautiful princess.",
+            height: 12,
+            weight: 2,
+            speed: 2,
+        }
+    },
+}
+
+// --1
+const marioGameJSON = JSON.stringify(marioGame);
+
+// --2
+const prettyMarioGameJSON = JSON.stringify(marioGame, null, 2);
+
+// --3
+console.log(`Without prettify: ${marioGameJSON}`);
+console.log(`With (null, 2) prettify: ${prettyMarioGameJSON}`);
